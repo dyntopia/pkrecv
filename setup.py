373ceefb20424dc7e78eab37bc2ad7a94d4792a8
Hans Jerry Illikainen <hji@dyntopia.com>
2018-12-25 10:03:30 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+import pkrecv
+
+setuptools.setup(
+    name="pkrecv",
+    version=pkrecv.__version__,
+    entry_points={
+        "console_scripts": ["pkrecv = pkrecv.__main__:main"]
+    }
+)
