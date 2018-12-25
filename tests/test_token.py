6315237ca1ca1c12a3cb925f8a1410d680971d8b
Hans Jerry Illikainen <hji@dyntopia.com>
2018-12-25 20:20:13 +0000

    Add tests for token.get_token()

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,9 +1,15 @@
-import hashlib
 import re
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
-from pkrecv.token import Token, TokenError, add_token, generate_token
+from pkrecv.token import (
+    Token,
+    TokenError,
+    add_token,
+    generate_token,
+    get_token,
+    sha256
+)
 
 from .helpers import FlaskTestCase
 
@@ -12,17 +18,51 @@ class AddTokenTest(FlaskTestCase):
     @patch("pkrecv.token.generate_token")  # type: ignore
     def test_sha256(self, mock: MagicMock) -> None:
         data = b"abcd"
-        sha256 = hashlib.sha256(data).hexdigest()
+        sha256sum = sha256(data)
 
         mock.return_value = data
         add_token("admin", "")
 
-        tokens = Token.query.filter_by(token=sha256).all()
+        tokens = Token.query.filter_by(token=sha256sum).all()
         self.assertEqual(len(tokens), 1)
 
     def test_invalid_role(self) -> None:
         with self.assertRaises(TokenError):
             add_token("abcd", "")
+
+
+class GetTokenTest(FlaskTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.first = add_token("admin", "desc1")
+        self.second = add_token("server", "desc2")
+
+    def test_id(self) -> None:
+        first = get_token(id=1)
+        self.assertEqual(first.id, 1)
+        self.assertEqual(first.token, sha256(bytes(self.first, "ascii")))
+        self.assertEqual(first.role, "admin")
+        self.assertEqual(first.description, "desc1")
+
+        second = get_token(id=2)
+        self.assertEqual(second.id, 2)
+        self.assertEqual(second.token, sha256(bytes(self.second, "ascii")))
+        self.assertEqual(second.role, "server")
+        self.assertEqual(second.description, "desc2")
+
+    def test_token(self) -> None:
+        first = get_token(token=self.first)
+        self.assertEqual(first.id, 1)
+        self.assertEqual(first.token, sha256(bytes(self.first, "ascii")))
+        self.assertEqual(first.role, "admin")
+        self.assertEqual(first.description, "desc1")
+
+        second = get_token(token=self.second)
+        self.assertEqual(second.id, 2)
+        self.assertEqual(second.token, sha256(bytes(self.second, "ascii")))
+        self.assertEqual(second.role, "server")
+        self.assertEqual(second.description, "desc2")
 
 
 class GenerateTokenTest(TestCase):
