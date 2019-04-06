6534306a7c32bbcf9b0a9c940467c41b7d36a77f
Hans Jerry Illikainen <hji@dyntopia.com>
2019-04-06 06:50:14 +0000

    setup: add install_requires

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
+import re
+import sys
+from typing import Iterator
+
 import setuptools
 
 import pkrecv
 
+
+def get_requirements(filename: str) -> Iterator[str]:
+    with open(filename, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line and not line.startswith("#"):
+                m = re.match(r"([a-zA-Z0-9-_]+)[ \t]==[ \t]+([0-9\.]+)", line)
+                if not m:
+                    sys.exit("ERROR: invalid requirements.txt")
+                yield "{0} >= {1}".format(*m.groups())
+
+
 setuptools.setup(
     name="pkrecv",
     version=pkrecv.__version__,
+    install_requires=list(get_requirements("requirements/requirements.txt")),
     entry_points={"console_scripts": ["pkrecv = pkrecv.__main__:main"]}
 )
